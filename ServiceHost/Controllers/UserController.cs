45cad4e41072bb204a7e8811c95f1f6fa5ffcd70
Yaser Balaghi <eng.balaghi@yahoo.com>
2019-11-02 16:52:07 +0330

    ...

--- ServiceHost/Controllers/UserController.cs
+++ ServiceHost/Controllers/UserController.cs
@@ -34,7 +34,7 @@ namespace ServiceHost.Controllers
 
         //   |  |
         //   |  |  Use Postman : 
-        //   |  |  First login and get a token for the user (admin) by calling GetToken Action and send parameters to it,
+        //   |  |  First login and get a token for the user (admin) by calling 'GetToken' Action and send parameters to it,
         //   |  |  then use the token you received to call the following actions...
         //   |  |   
         //   \  /
